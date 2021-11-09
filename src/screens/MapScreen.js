bbe3c983a20602eb24e783fdcb9e2f933c1fd8e9
JAYA <mavuleti.satya@gmail.com>
2021-11-09 15:38:33 +0100

    navigation tabs added

---
+++ src/screens/MapScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const MapScreen = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Map Screen</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default MapScreen
+
+const styles = StyleSheet.create({})
