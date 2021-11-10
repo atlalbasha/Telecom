2a87de60d60b115337353fd16972f89bcd2401d9
JAYA <mavuleti.satya@gmail.com>
2021-11-10 17:19:31 +0100

    Google Map Added

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import * as React from 'react'
+import MapView, { Callout, Marker } from 'react-native-maps'
+import { StyleSheet, View, Dimensions, Text } from 'react-native'
 
 const MapScreen = () => {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Map Screen</Text>
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 57.70887,
+            longitude: 11.97456,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: 57.70887,
+              longitude: 11.97456
+            }}
+          >
+            <Callout>
+              <Text>Gothenburg</Text>
+            </Callout>
+          </Marker>
+        </MapView>
       </View>
     </SafeAreaView>
   )
@@ -14,4 +34,9 @@ const MapScreen = () => {
 
 export default MapScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  }
+})
