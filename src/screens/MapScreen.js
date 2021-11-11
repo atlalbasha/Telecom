7be54940f12a552e6b555b84086ee74585fa009d
JAYA <mavuleti.satya@gmail.com>
2021-11-11 10:20:52 +0100

    Hear Map Added

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,40 +1,53 @@
-import { SafeAreaView } from 'react-native-safe-area-context'
 import * as React from 'react'
-import MapView, { Callout, Marker } from 'react-native-maps'
+import MapView, { Callout, Heatmap, Marker } from 'react-native-maps'
 import { StyleSheet, View, Dimensions, Text } from 'react-native'
+import { Colors } from 'react-native/Libraries/NewAppScreen'
 
-const MapScreen = () => {
+export default function App() {
+  const area = [
+    { latitude: 57.70887, longitude: 11.97456, weight: 2 },
+    { latitude: 58.283489, longitude: 12.285821, weight: 2 }
+  ]
+  const areaColors = ['blue', 'red', 'orange']
+  const startPoints = [0.01, 0.5, 0.75]
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <MapView
-          style={styles.map}
-          initialRegion={{
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 57.70887,
+          longitude: 11.97456,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.01
+        }}
+      >
+        <Heatmap
+          points={area}
+          radius={20}
+          gradient={{ colors: areaColors, startPoints }}
+        />
+        <Marker
+          coordinate={{
             latitude: 57.70887,
-            longitude: 11.97456,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
+            longitude: 11.97456
           }}
         >
-          <Marker
-            coordinate={{
-              latitude: 57.70887,
-              longitude: 11.97456
-            }}
-          >
-            <Callout>
-              <Text>Gothenburg</Text>
-            </Callout>
-          </Marker>
-        </MapView>
-      </View>
-    </SafeAreaView>
+          <Callout>
+            <Text>Gothenburg</Text>
+          </Callout>
+        </Marker>
+      </MapView>
+    </View>
   )
 }
 
-export default MapScreen
-
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height
