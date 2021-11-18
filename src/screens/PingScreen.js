4727745c31f09b22fa4fe8304895d7cf8eb0a23b
RomanDivkovic <divkovicroman@gmail.com>
2021-11-18 19:40:43 +0100

    Added a textinput to change url that user can try out to get committed
    - Need to finish clock(timer), got it counting before but not how long it takes
    - Also need to impliment the ping each 50ms, 100ms, 20ms, 500ms, 1000ms​

--- src/screens/PingScreen.js
+++ src/screens/PingScreen.js
@@ -1,16 +1,22 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { Pressable, StyleSheet, Text, View, FlatList } from 'react-native'
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TextInput
+} from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import axios from 'axios'
 
 export default function PingScreen() {
   const [result, setResult] = useState([])
 
-  //Kanske behöver använda att fylla array med requests eller sekunder det tar..
-  // const pingArray = useContext([])
+  const [text, onTextChange] = React.useState('')
 
   // Det ska kunna göras 3st ping request en gång alternativt kontinuerligt med följande interval: ​
-  // 50ms, 100ms, 200ms, 500ms, 1000ms​
+  // 50ms, 100ms, 20ms, 500ms, 1000ms​
   // Lägga en if-sats och stoppa ifall number === tex 2000ms
 
   return (
@@ -18,6 +24,32 @@ export default function PingScreen() {
       <Text style={{ textAlign: 'center', fontSize: 20 }}>
         I'm the ping screen
       </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onTextChange}
+        value={text}
+        placeholder="Write a url to search"
+        keyboardType="url"
+      />
+      <Pressable
+        value={text}
+        onPress={() => {
+          getUrl(text)
+        }}
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed ? 'lightgrey' : '#0077b6'
+          },
+          styles.viewButton
+        ]}
+      >
+        <Text style={styles.buttonText}>Send new url</Text>
+      </Pressable>
+      <FlatList data={result}>
+        <View style={styles.item}>
+          <Text style={styles.text}>Type: {result}</Text>
+        </View>
+      </FlatList>
       <FlatList
         data={result}
         renderItem={({ item }) => (
@@ -44,13 +76,36 @@ export default function PingScreen() {
       </Pressable>
     </SafeAreaView>
   )
+
   function getPing() {
     const searchApi = async () => {
       const response = await axios.get('https://www.google.se/')
       setResult(response.status)
       console.log(result)
+      // if (result) {
+      //   const timer = count - now
+      //   console.log('My timer: ', timer)
+      // }
     }
     searchApi()
+  }
+  function getUrl(url) {
+    const searchApi = async () => {
+      const response = await axios.get(url)
+      setResult(response.data.jokes)
+      console.log(result)
+      // if (result) {
+      //   let timer = countDownDate - new Date().getTime
+      //   console.log('My timer: ', timer)
+      //   console.log(result)
+      //   return result
+      // }
+    }
+    searchApi()
+  }
+  function setTimer() {
+    var count = new Date(countDownDate).getTime()
+    var now = new Date().getTime()
   }
 }
 
@@ -80,5 +135,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#0000',
     fontSize: 15,
     textAlign: 'center'
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10
   }
 })
