e98cc5eb5ae27ba5dfc1c652145e9b2497e30894
RomanDivkovic <divkovicroman@gmail.com>
2021-11-19 15:01:50 +0100

    Code clean up

--- src/screens/PingScreen.js
+++ src/screens/PingScreen.js
@@ -10,24 +10,12 @@ import {
 import { SafeAreaView } from 'react-native-safe-area-context'
 import axios from 'axios'
 
+//Det ska kunna göras 3st ping request en gång alternativt kontinuerligt med följande interval: ​
+//50ms, 100ms, 200ms, 500ms, 1000ms
+
 export default function PingScreen() {
-  const [result, setResult] = useState([])
   const [urlResult, setUrlResult] = useState([])
-
   const [text, onTextChange] = React.useState('')
-
-  const [counter, setCounter] = React.useState(5)
-  // Klockar ner från 5msek måste integrera med ping koden  50ms, 100ms, 20ms, 500ms, 1000ms​
-  useEffect(() => {
-    const timer =
-      counter > 0 && setInterval(() => setCounter(counter - 1), 1000)
-    getPing()
-    return () => clearInterval(timer)
-  }, [counter])
-
-  // Det ska kunna göras 3st ping request en gång alternativt kontinuerligt med följande interval: ​
-  // 50ms, 100ms, 200ms, 500ms, 1000ms​
-  // Lägga en if-sats och stoppa ifall number === tex 2000ms
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: '#14213d' }}>
@@ -41,19 +29,6 @@ export default function PingScreen() {
       >
         Ping Screen
       </Text>
-      <Text style={{ color: 'grey' }}>Countdown: {counter}</Text>
-
-      {/* <FlatList
-          data={result}
-          renderItem={({ item }) => (
-            <View style={styles.item}>
-              <Text style={styles.text}>Type: Ping: {item}</Text>
-            </View>
-          )}
-          keyExtractor={(joke) => joke.id.toString()}
-        /> */}
-
-      <Text style={styles.text}>Ping Code: {result}</Text>
       <Text style={styles.text}>URL Code: {urlResult}</Text>
 
       <TextInput
@@ -77,31 +52,9 @@ export default function PingScreen() {
       >
         <Text style={styles.buttonText}>Send new url</Text>
       </Pressable>
-      <Pressable
-        onPress={() => {
-          getPing()
-          console.log('', result)
-        }}
-        style={({ pressed }) => [
-          {
-            backgroundColor: pressed ? 'lightgrey' : '#0077b6'
-          },
-          styles.viewButton
-        ]}
-      >
-        <Text style={styles.buttonText}>Ping some info</Text>
-      </Pressable>
     </SafeAreaView>
   )
 
-  function getPing() {
-    const searchApi = async () => {
-      const response = await axios.get('https://www.amazon.com/')
-      setResult(response.status)
-      console.log('Ping result: ', result)
-    }
-    searchApi()
-  }
   function getUrl(url) {
     const searchApi = async () => {
       const response = await axios.get('https://' + url)
@@ -117,7 +70,8 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: 'white',
     textAlign: 'center',
-    top: 10
+    fontWeight: 'bold',
+    top: Platform.OS === 'ios' ? 10 : 0
   },
   viewButton: {
     minHeight: 50,
