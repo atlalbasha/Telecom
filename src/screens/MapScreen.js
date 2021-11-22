95c795bc7a90b8c395c5789d399308aa33834bb3
JAYA <mavuleti.satya@gmail.com>
2021-11-22 15:18:50 +0100

    fixed menuitems code

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -7,6 +7,7 @@ import { silverTheme } from './Data.js/MapStyle2'
 import { Appbar } from 'react-native-paper'
 import { DataContext } from './shared/utils/DataContext'
 import { Menu, MenuItem, MenuDivider } from 'react-native-material-menu'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { useContext } from 'react'
 export default function App() {
@@ -14,7 +15,6 @@ export default function App() {
   const [data, setData] = useContext(DataContext)
   const [visible, setVisible] = useState(false)
 
-  console.log(data)
   const areaColors = ['green', 'orange', 'red']
 
   const startPoints = [0.01, 0.5, 0.75]
@@ -31,58 +31,61 @@ export default function App() {
   const hideMenu = () => setVisible(false)
 
   return (
-    <View>
-      <Appbar.Header style={styles.header}>
-        <Appbar.Content title="MapView" />
+    <SafeAreaView>
+      <View>
+        <Appbar style={styles.header}>
+          <Appbar.Content title="MapView" />
 
-        <Menu
-          visible={visible}
-          anchor={
-            <Text
-              style={{ fontSize: 20, fontWeight: 'bold', color: 'white' }}
-              onPress={showMenu}
-            >
-              Styles
-            </Text>
-          }
+          <Menu
+            visible={visible}
+            anchor={
+              <Text
+                style={{ fontWeight: 'bold', fontSize: 20, color: 'white' }}
+                onPress={showMenu}
+              >
+                Styles
+              </Text>
+            }
+            onRequestClose={hideMenu}
+          >
+            <MenuItem onPress={changeTheme}>Dark</MenuItem>
+            <MenuDivider />
+            <MenuItem onPress={changeThemeSilver}>Silver</MenuItem>
+
+            <MenuDivider />
+            <MenuItem onPress={changeThemeStandard}>Standard</MenuItem>
+          </Menu>
+        </Appbar>
+        <MapView
+          style={{ flex: 1 }}
+          style={styles.map}
+          provider={PROVIDER_GOOGLE}
+          customMapStyle={customMapStyle}
+          initialRegion={{
+            latitude: 57.70887,
+            longitude: 11.97456,
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.01
+          }}
         >
-          <MenuItem onPress={changeTheme}>Dark</MenuItem>
-          <MenuDivider />
-          <MenuItem onPress={changeThemeSilver}>Silver</MenuItem>
-
-          <MenuDivider />
-          <MenuItem onPress={changeThemeStandard}>Standard</MenuItem>
-        </Menu>
-      </Appbar.Header>
-      <MapView
-        style={{ flex: 1 }}
-        style={styles.map}
-        provider={PROVIDER_GOOGLE}
-        customMapStyle={customMapStyle}
-        initialRegion={{
-          latitude: 57.70887,
-          longitude: 11.97456,
-          latitudeDelta: 0.05,
-          longitudeDelta: 0.01
-        }}
-      >
-        <Heatmap
-          points={data}
-          opacity={1}
-          radius={20}
-          maxIntensity={100}
-          gradientSmoothing={10}
-          heatmapMode={'POINTS_DENSITY'}
-          gradient={{ colors: areaColors, startPoints }}
-        ></Heatmap>
-      </MapView>
-    </View>
+          <Heatmap
+            points={data}
+            opacity={1}
+            radius={20}
+            maxIntensity={100}
+            gradientSmoothing={10}
+            heatmapMode={'POINTS_DENSITY'}
+            gradient={{ colors: areaColors, startPoints }}
+          ></Heatmap>
+        </MapView>
+      </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   header: {
-    backgroundColor: 'teal'
+    backgroundColor: '#252b41'
   },
   map: {
     width: Dimensions.get('window').width,
