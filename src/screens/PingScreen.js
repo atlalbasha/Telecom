23f02085053b1b3eacd105d9dfae0520663cc78f
RomanDivkovic <divkovicroman@gmail.com>
2021-11-22 09:19:56 +0100

    Added Timer npm package, displaying on screen and starts when calling axios
    - Need to make it stop and reset now as well

--- src/screens/PingScreen.js
+++ src/screens/PingScreen.js
@@ -3,19 +3,22 @@ import {
   Pressable,
   StyleSheet,
   Text,
+  TextInput,
   View,
-  FlatList,
-  TextInput
+  TouchableHighlight
 } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import axios from 'axios'
+import { Stopwatch, Timer } from 'react-native-stopwatch-timer'
 
-//Det ska kunna göras 3st ping request en gång alternativt kontinuerligt med följande interval: ​
-//50ms, 100ms, 200ms, 500ms, 1000ms
-
+//Det ska kunna göras 3st ping request en gång
 export default function PingScreen() {
   const [urlResult, setUrlResult] = useState([])
-  const [text, onTextChange] = React.useState('')
+  const [text, onTextChange] = useState('')
+
+  //Timer
+  const [isStopwatchStart, setIsStopwatchStart] = useState(false)
+  const [resetStopwatch, setResetStopwatch] = useState(false)
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: '#14213d' }}>
@@ -29,8 +32,25 @@ export default function PingScreen() {
       >
         Ping Screen
       </Text>
+      <View style={options.container}>
+        <View style={options.sectionStyle}>
+          <Stopwatch
+            laps
+            msecs
+            start={isStopwatchStart}
+            //To start
+            reset={resetStopwatch}
+            //To reset
+            options={options}
+            //options for the styling
+            getTime={(time) => {
+              getUrl()
+              console.log(time)
+            }}
+          />
+        </View>
+      </View>
       <Text style={styles.text}>URL Code: {urlResult}</Text>
-
       <TextInput
         style={styles.input}
         onChangeText={onTextChange}
@@ -42,6 +62,7 @@ export default function PingScreen() {
         value={text}
         onPress={() => {
           getUrl(text)
+          setIsStopwatchStart(!isStopwatchStart)
         }}
         style={({ pressed }) => [
           {
@@ -56,9 +77,15 @@ export default function PingScreen() {
   )
 
   function getUrl(url) {
+    // start a timer in button press
     const searchApi = async () => {
       const response = await axios.get('https://' + url)
       setUrlResult(response.status)
+      // end timer when request finished
+      if (urlResult === '200') {
+        setIsStopwatchStart(!isStopwatchStart)
+        setResetStopwatch(true)
+      }
       console.log('URL result ', urlResult)
     }
     searchApi()
@@ -100,3 +127,18 @@ const styles = StyleSheet.create({
     color: 'white'
   }
 })
+
+const options = {
+  container: {
+    alignSelf: 'center',
+    backgroundColor: '#FF0000',
+    padding: 5,
+    borderRadius: 5,
+    width: 200
+  },
+  text: {
+    fontSize: 25,
+    color: '#FFF',
+    marginLeft: 7
+  }
+}
