b8ef714320c281f1a8d0c64430e71e983008a28e
RomanDivkovic <divkovicroman@gmail.com>
2021-11-24 11:38:38 +0100

    Added all data to list but theres a bug to fix still

--- src/screens/PingScreen.js
+++ src/screens/PingScreen.js
@@ -10,88 +10,102 @@ import {
 } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import axios from 'axios'
-import { Stopwatch } from 'react-native-stopwatch-timer'
+import { Stopwatch, Timer } from 'react-native-stopwatch-timer'
 
 export default function PingScreen() {
   const [urlResult, setUrlResult] = useState([])
   const [text, onTextChange] = useState('')
   let [result] = useState([])
+  let firstResult = useState([])
+  let time = useState('')
 
   const [isStopwatchStart, setIsStopwatchStart] = useState(false)
   const [resetStopwatch, setResetStopwatch] = useState(false)
+  const [totTime, setTotTime] = useState('')
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#14213d' }}>
-      <View style={options.container}>
-        <Text
-          style={{
-            textAlign: 'center',
-            fontSize: 20,
-            fontWeight: 'bold',
-            color: 'white'
-          }}
-        >
-          Timer for request
-        </Text>
-        <View>
-          <Stopwatch
-            laps
-            msecs
-            start={isStopwatchStart}
-            reset={resetStopwatch}
-            options={options}
-          />
-        </View>
-      </View>
-      <Text style={styles.text}>Code: {urlResult}</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={onTextChange}
-        value={text}
-        placeholder="Search url for ping"
-        placeholderTextColor="#fff"
-        // eller default till android
-        keyboardType={Platform.OS === 'ios' ? 'url' : 'email-address'}
-      />
-      <Pressable
-        onPress={() => {
-          getUrl(text)
-          setIsStopwatchStart(!isStopwatchStart)
-          setResetStopwatch(false)
-        }}
-        style={({ pressed }) => [
-          {
-            backgroundColor: pressed ? 'lightgrey' : '#0077b6'
-          },
-          styles.viewButton
-        ]}
-      >
-        <Text style={styles.buttonText}>Search</Text>
-      </Pressable>
-      <Pressable
-        onPress={() => {
-          setResetStopwatch(!resetStopwatch)
-        }}
-        style={({ pressed }) => [
-          {
-            backgroundColor: pressed ? 'lightgrey' : '#0077b6'
-          },
-          styles.viewButton
-        ]}
-      >
-        <Text style={styles.buttonText}>RESET</Text>
-      </Pressable>
-      <FlatList
-        data={result}
-        renderItem={({ item }) => (
-          <View style={styles.item}>
-            <Text style={styles.listText}>Type: {item.status}</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ backgroundColor: '#252b41', height: '100%', padding: 16 }}>
+        <View style={options.container}>
+          <Text
+            style={{
+              textAlign: 'center',
+              fontSize: 20,
+              fontWeight: 'bold',
+              color: 'white'
+            }}
+          >
+            Timer for request
+          </Text>
+          <View>
+            <Stopwatch
+              laps
+              msecs
+              start={isStopwatchStart}
+              reset={resetStopwatch}
+              options={options}
+              getTime={getFormattedTime}
+            />
           </View>
-        )}
-        keyExtractor={(joke) => joke.id}
-      />
+        </View>
+        <Text style={styles.text}>Code: {urlResult}</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onTextChange}
+          value={text}
+          placeholder="Search url for ping"
+          placeholderTextColor="#fff"
+          // eller default till android
+          keyboardType={Platform.OS === 'ios' ? 'url' : 'email-address'}
+        />
+        <Pressable
+          onPress={() => {
+            getUrl(text)
+            setIsStopwatchStart(!isStopwatchStart)
+            setResetStopwatch(false)
+          }}
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed ? 'lightgrey' : '#0077b6'
+            },
+            styles.viewButton
+          ]}
+        >
+          <Text style={styles.buttonText}>Search</Text>
+        </Pressable>
+        <Pressable
+          onPress={() => {
+            setResetStopwatch(!resetStopwatch)
+          }}
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed ? 'lightgrey' : '#0077b6'
+            },
+            styles.viewButton
+          ]}
+        >
+          <Text style={styles.buttonText}>RESET</Text>
+        </Pressable>
+        <FlatList
+          data={result}
+          renderItem={({ item }) => (
+            <View style={styles.item}>
+              <Text style={styles.listText}>
+                Request: {item.text}, Time: {item.status} & status: {item.code}
+              </Text>
+            </View>
+          )}
+          keyExtractor={(text) => text.id}
+        />
+      </View>
     </SafeAreaView>
   )
+
+  function getFormattedTime(time) {
+    if (time !== '00:00:00:000') {
+      setTotTime(time)
+    }
+  }
 
   function getUrl(url) {
     const searchApi = async () => {
@@ -100,22 +114,21 @@ export default function PingScreen() {
         setUrlResult(response.status)
         result.push({
           id: result.length,
-          status: response.status
+          text: url,
+          status: totTime,
+          code: response.status
         })
-        // end timer when request finished
         setIsStopwatchStart(false)
       } catch (error) {
         if (axios.isCancel(error)) {
           console.log('Data fetching cancelled')
         } else {
-          // Handle error
           setUrlResult(error.toString())
           setResetStopwatch(false)
           setIsStopwatchStart(false)
         }
       }
     }
-
     searchApi()
     searchApi()
     searchApi()
