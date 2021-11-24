b6618f1c49c80214cb8d5bbcbcb4f69209cefca7
atlalbasha <atlal.basha@gmail.com>
2021-11-24 13:13:31 +0100

    on/off ping data

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react'
-import { StyleSheet, Text, View, Switch } from 'react-native'
+import { StyleSheet, Text, View, Switch, Button } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import * as Location from 'expo-location'
 import NetInfo from '@react-native-community/netinfo'
@@ -9,56 +9,66 @@ import LocationInfo from './components/LocationInfo'
 import { DataContext } from './shared/utils/DataContext'
 
 const HomeScreen = () => {
-
   const { data } = useContext(DataContext)
   const [dataValues, setDataValues] = data
   const [randomNumber, setRandomNumber] = useState(0)
 
-
+  const [client, setClient] = useState(null)
   const [netInfoData, setNetInfoData] = useState()
   const [location, setLocation] = useState(null)
   //SWITCH
   const [isEnabled, setIsEnabled] = useState(true)
-  const toggleSwitch = () => setIsEnabled((previousState) => !previousState)
+  const toggleSwitch = () => {
+    setIsEnabled((previousState) => !previousState)
+  }
 
   const getRandomNumber = () => {
     setRandomNumber(Math.floor(Math.random() * 100) + 1)
   }
-
-  useEffect(async () => {
+  const getPermissions = async () => {
     let { status } = await Location.requestForegroundPermissionsAsync()
 
     if (status !== 'granted') {
       setErrorMessage('Permission to access location was denied')
       return
     }
-
+  }
+  const getNetInfo = async () => {
     NetInfo.addEventListener((state) => {
       setNetInfoData(state)
     })
+  }
+  const getLocation = async () => {
+    const loc1 = await Location.watchPositionAsync(
+      {
+        accuracy: Location.Accuracy.Highest,
+        distanceInterval: 1,
+        timeInterval: 1000
+      },
+      (loc) => {
+        setLocation(loc)
+        getRandomNumber()
+        setDataValues((prevLocation) => [
+          ...prevLocation,
+          {
+            longitude: loc.coords.longitude,
+            latitude: loc.coords.latitude,
+            weight: randomNumber
+          }
+        ])
+      }
+    )
 
-    console.log(isEnabled)
-    if (isEnabled === true) {
-      console.log(isEnabled)
-      await Location.watchPositionAsync(
-        {
-          accuracy: Location.Accuracy.Highest,
-          distanceInterval: 1,
-          timeInterval: 1000
-        },
-        (loc) => {
-          setLocation(loc)
-          getRandomNumber()
-          setDataValues((prevLocation) => [
-            ...prevLocation,
-            {
-              longitude: loc.coords.longitude,
-              latitude: loc.coords.latitude,
-              weight: randomNumber
-            }
-          ])
-        }
-      )
+    setClient(loc1)
+  }
+
+  useEffect(() => {
+    getPermissions()
+    getNetInfo()
+    if (isEnabled) {
+      getLocation()
+    } else {
+      client.remove()
     }
   }, [isEnabled])
 
